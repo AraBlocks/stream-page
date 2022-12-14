a04440d2cb44181489825125106a68cc4639c417
Kevin Faaborg <kevin@ara.one>
2022-12-14 15:12:57 -0700

    added date and version number

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, React! 2022dec14 v30. Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
